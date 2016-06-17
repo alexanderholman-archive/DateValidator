ea4e7b308ca9f4a6975148cce7cd5ad06fb525e3
alexanderholman <alexander@holman.org.uk>
2016-06-17 20:05:25 +0100

    Result class used by the Date Validator class

---
+++ src/result.php
@@ -0,0 +1,41 @@
+<?php
+
+class Result {
+
+    /**
+     * @var string
+     */
+    private $message = '';
+
+    /**
+     * @var bool
+     */
+    private $valid = false;
+
+    /**
+     * @param $message
+     */
+    public function setMessage( $message ) {
+        if ( !is_string( $message ) ) return false;
+        $this->message = $message;
+        return true;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage() {
+        return $this->message;
+    }
+
+    public function setValidity( $validity ) {
+        if ( !is_bool( $validity ) ) return false;
+        $this->valid = $validity;
+        return true;
+    }
+
+    public function isValid() {
+        return $this->valid;
+    }
+
+}
