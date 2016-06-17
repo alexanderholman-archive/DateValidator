5fcaf1adc0f8b1540563400ec371045fd26a7a98
alexanderholman <alexander@holman.org.uk>
2016-06-17 20:03:55 +0100

    example for testing

---
+++ example/getting-started.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once '../src/result.php';
+require_once '../src/date-validator.php';
+
+$automate = false;
+
+$message = 'Hi! if you\'re reading this the date is valid and matches the pattern DD/MM/YYYY';
+
+$dateString = '03/12/1999';            // Valid
+//$dateString = '03/12/2999';            // Valid
+//$dateString = '3/12/1999';             // Error
+//$dateString = '12/31/1999';            // Error
+//$dateString = '12/31/1999/03/12/1999'; // Error
+//$dateString = '03/31/99';              // Error
+//$dateString = '3/31/99';               // Error
+//$dateString = '';                      // Error
+//$dateString = null;                    // Error
+
+$result = DateValidator::validateHistoricalDate($dateString);
+
+if (!$result->isValid()) {
+
+    $message = $result->getMessage();
+
+}
+
+echo $automate ? 'Function automated test result: ' . ( DateValidator::testValidateHistoricalDate() ? 'working' : 'error' ) : $message;
