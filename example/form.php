aa36302a18a0fb69cf31eed2fa41b05cebbd38aa
alexanderholman <alexander@holman.org.uk>
2016-06-27 12:39:59 +0200

    add basic html form example

---
+++ example/form.php
@@ -0,0 +1,37 @@
+<?php
+$inputDate = '';
+$message = false;
+if ( isset( $_POST['date'] ) ) {
+	require_once '../src/result.php';
+	require_once '../src/date-validator.php';
+	$inputDate = $_POST['date'];
+	$result = DateValidator::validateHistoricalDate( $inputDate, true );
+	if ( $result->isValid() ) {
+		$message = "Date ($inputDate) is valid";
+		$inputDate = '';
+	} else {
+		if ( $inputDate == 'null' ) $inputDate = '';
+		$message = $result->getMessage();
+	}
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Example</title>
+</head>
+<body>
+	<h1>Example form</h1>
+<?php 
+	if ($message) {
+		print "\t<h3>The results</h3>\n\t<p>$message</p>\n";
+	}
+?>
+	<h3>The form</h3>
+	<p>Input a date in the format 'DD/MM/YYYY'</p>
+	<form action="./form.php" method="post">
+		<input type="text" name="date" value="<?php echo $inputDate; ?>" />
+		<input type="submit" value="Validate date" />
+	</form>
+</body>
+</html>
